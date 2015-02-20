5363504579b4bf865cf2d0da9aeeb4553863ce74
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2015-02-20 18:10:46 +0000

    Fix OpenSSL support by renaming CONF to CI_CONF
    Import from FreeBSD ports.
    
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@1140 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- aserver.c
+++ aserver.c
@@ -102,9 +102,9 @@ int main(int argc, char **argv)
      }
      commands_init();
 
-     if (!(CONF.MAGIC_DB = ci_magic_db_load(CONF.magics_file))) {
+     if (!(CI_CONF.MAGIC_DB = ci_magic_db_load(CI_CONF.magics_file))) {
           ci_debug_printf(1, "Can not load magic file %s!!!\n",
-                          CONF.magics_file);
+                          CI_CONF.magics_file);
      }
      init_conf_tables();
      request_stats_init();
@@ -115,22 +115,22 @@ int main(int argc, char **argv)
      ci_debug_printf(2, "My hostname is:%s\n", MY_HOSTNAME);
 
 #if ! defined(_WIN32)
-     if (is_icap_running(CONF.PIDFILE)) {
+     if (is_icap_running(CI_CONF.PIDFILE)) {
           ci_debug_printf(1, "c-icap server already running!\n");
           exit(-1);
      }
      if (DAEMON_MODE)
           run_as_daemon();
-     if (!set_running_permissions(CONF.RUN_USER, CONF.RUN_GROUP))
+     if (!set_running_permissions(CI_CONF.RUN_USER, CI_CONF.RUN_GROUP))
           exit(-1);
-     store_pid(CONF.PIDFILE);
+     store_pid(CI_CONF.PIDFILE);
 #endif
 
      if (!log_open()) {
           ci_debug_printf(1, "Can not init loggers. Exiting.....\n");
           exit(-1);
      }
-     if (!init_server(CONF.PORT, &(CONF.PROTOCOL_FAMILY)))
+     if (!init_server(CI_CONF.PORT, &(CI_CONF.PROTOCOL_FAMILY)))
           return -1;
      post_init_modules();
      post_init_services();
