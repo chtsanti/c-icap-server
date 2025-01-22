acfb5b740eaab3b4498183c4809edc6c72601639
Christos Tsantilas <christos@chtsanti.net>
2025-01-22 11:48:06 +0200

    Fix test/test_shared_locking test to run in ms-windows environment

--- tests/test_shared_locking.c
+++ tests/test_shared_locking.c
@@ -6,15 +6,20 @@
 #include "client.h"
 #include "proc_mutex.h"
 #include "shared_mem.h"
-
+#if !defined(_WIN32)
 #include <sys/wait.h>
+#endif
 
 int LOOPS = 10000;
 int PROCS = 50;
 int THREADS = 1;
 int USE_DEBUG_LEVEL = -1;
 int EMULATE_CRASHES = 0;
+#if defined(_WIN32)
+const char *SCHEME = "win32";
+#else
 const char *SCHEME = "pthread";
+#endif
 
 static struct ci_options_entry options[] = {
     {
@@ -23,7 +28,14 @@ static struct ci_options_entry options[] = {
     },
     {
         "-s", "locking_scheme", &SCHEME, ci_cfg_set_str,
+#if defined(_WIN32)
+        "win32"
+#else
         "posix|sysv|file"
+#if defined(__CYGWIN__)
+        "|win32"
+#endif
+#endif
     },
     {
         "-l", "loops", &LOOPS, ci_cfg_set_int,
@@ -65,12 +77,10 @@ ci_thread_mutex_t mtx;
 ci_thread_cond_t cnd;
 int Started = 0;
 
-#define CLOCK_TIME_DIFF_micro(tsstop, tsstart) ((tsstop.tv_sec - tsstart.tv_sec) * 1000000) + ((tsstop.tv_nsec - tsstart.tv_nsec) / 1000)
-
 void thread()
 {
     int i;
-    struct timespec start, stop;
+    ci_clock_time_t start, stop;
     if (THREADS > 1) {
         /*Wait to synchronize*/
         ci_thread_mutex_lock(&mtx);
@@ -78,7 +88,7 @@ void thread()
         ci_thread_cond_wait(&cnd, &mtx);
         ci_thread_mutex_unlock(&mtx);
     }
-    clock_gettime (CLOCK_REALTIME, &start);
+    ci_clock_time_get(&start);
     for(i = 0; i < LOOPS; ++i) {
         assert(ci_proc_mutex_lock(&stats_mutex));
         Stats->c += 1;
@@ -96,15 +106,21 @@ void thread()
         }
         ci_proc_mutex_unlock(&stats_mutex);
     }
-    clock_gettime (CLOCK_REALTIME, &stop);
+    ci_clock_time_get(&stop);
     ci_thread_mutex_lock(&mtx);
-    Stats->times[KidId] += CLOCK_TIME_DIFF_micro(stop, start);
+    Stats->times[KidId] += ci_clock_time_diff_micro(&stop, &start);
     ci_thread_mutex_unlock(&mtx);
 }
 
 void run_child(int id) {
     KidId = id;
+    ci_debug_printf(3, "Start kid %d\n", id);
     Stats = ci_shared_mem_attach(&sid);
+    if (!Stats) {
+        ci_debug_printf(1, "Error attaching memory block at %s\n", sid.name);
+        return;
+    }
+    ci_debug_printf(2, "The kid %d will write to shared mem: %s\n", KidId, sid.name);
     Stats->times[KidId] = 0;
     ci_thread_mutex_init(&mtx);
     ci_thread_cond_init(&cnd);
@@ -122,7 +138,7 @@ void run_child(int id) {
                              (void *) NULL /*data*/);
         }
         while(Started < THREADS) usleep(100);
-        usleep(1000);
+        usleep(1000000);
         ci_thread_cond_broadcast(&cnd);
         for (i = 0; i < THREADS; i++) {
             ci_thread_join(threads[i]);
@@ -132,12 +148,78 @@ void run_child(int id) {
     ci_debug_printf(2, "Loops took %d microsecs\n", Stats->times[id]);
     ci_thread_mutex_destroy(&mtx);
     ci_thread_cond_destroy(&cnd);
+    ci_shared_mem_detach(&sid);
+}
+
+#if defined(_WIN32)
+typedef PROCESS_INFORMATION _PINFO;
+#else
+typedef pid_t _PINFO;
+#endif
+
+int ISKID = 0;
+void _BUILD_KID(int id, _PINFO *pi, int argc, char *argv[])
+{
+#if defined(_WIN32)
+    int bytes, i;
+    char _CMD[2048];
+    bytes = snprintf(_CMD, sizeof(_CMD), "%s --kid %d", argv[0], id);
+    for(i = 1; i < argc && bytes < sizeof(_CMD); ++i) {
+        bytes += snprintf(_CMD + bytes, sizeof(_CMD) - bytes, " %s", argv[i]);
+    }
+    ci_debug_printf(3, "Run kid command: %s\n", _CMD);
+    assert(bytes < sizeof(_CMD));
+    STARTUPINFO si;
+    ZeroMemory( &si, sizeof(si) );
+    si.cb = sizeof(si);
+    ZeroMemory( pi, sizeof(_PINFO) );
+    /* Start the child process.*/
+    if(!CreateProcess(NULL, _CMD, NULL, NULL, FALSE, 0, NULL, NULL, &si, pi )) {
+        ci_debug_printf(1, "CreateProcess failed (%d).\n", GetLastError());
+        pi->hProcess = NULL;
+        pi->hThread  = NULL;
+    }
+    ci_debug_printf(3, "OK process created\n");
+#else
+    if ((*pi = fork()) == 0) {
+        ISKID = 1;
+        KidId = id;
+    }
+#endif
+}
+
+void _WAIT_KID(_PINFO *pi)
+{
+#if defined(_WIN32)
+    WaitForSingleObject(pi->hProcess, INFINITE );
+    CloseHandle(pi->hProcess );
+    CloseHandle(pi->hThread );
+    ci_debug_printf(3, "child %p terminated\n", pi->hProcess);
+#else
+    int pid, status;
+    // just wait for any kid;
+    pid = wait(&status);
+    if (!WIFEXITED(status)) {
+        ci_debug_printf(1, "Child %d abnormal termination with status %d\nCheck mutex states\n", pid, status);
+        ci_proc_mutex_recover_after_crash();
+    } else {
+        ci_debug_printf(4, "Child %d terminated with status %d\n", pid, status);
+../    }
+#endif
 }
 
 int main(int argc, char *argv[])
 {
+    CI_DEBUG_STDOUT = 1;
+#if defined(_WIN32)
+    if (argc > 2 && strcmp(argv[1], "--kid") == 0) {
+        ISKID = 1;
+        KidId = atoi(argv[2]);
+        argv += 2;
+        argc -= 2;
+    }
+#endif
     ci_client_library_init();
-
     __log_error = (void (*)(void *, const char *, ...)) log_errors;     /*set c-icap library log  function */
 
     if (!ci_args_apply(argc, argv, options)) {
@@ -159,21 +241,20 @@ int main(int argc, char *argv[])
     }
     ci_proc_mutex_init(&stats_mutex, "stats");
     int i;
-    for(i = 0; i < PROCS; i++) {
-        if (fork() == 0) {
-            run_child(i);
-            exit(0);
-        }
+    _PINFO *processes = calloc(PROCS, sizeof(_PINFO));
+    for(i = 0; i < PROCS && !ISKID; i++) {
+        _BUILD_KID(i, &processes[i], argc, argv);
     }
+
+    if (ISKID) {
+        ci_debug_printf(4, "Run runchild\n");
+        run_child(KidId);
+        exit(0);
+    }
+
     for(i = 0; i < PROCS; i++) {
-        int pid, status;
-        pid = wait(&status);
-        if (!WIFEXITED(status)) {
-            ci_debug_printf(1, "Child %d abnormal termination with status %d\nCheck mutex states\n", pid, status);
-            ci_proc_mutex_recover_after_crash();
-        } else {
-            ci_debug_printf(4, "Child %d terminated with status %d\n", pid, status);
-        }
+        if (processes[i].hProcess != NULL)
+            _WAIT_KID(&processes[i]);
     }
 
     ci_proc_mutex_destroy(&stats_mutex);
